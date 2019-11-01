a7d7990a0264d9845fcec453c4523ff5c04179c9
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-01 09:06:37 +0000

    API set up done - server listening on port

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const helmet = require("helmet");
 const server = express();
 
 server.use(express.json());
-server.use(cors);
+server.use(cors());
 server.use(helmet());
 
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is working" });
+  });
+  
 module.exports = server;
