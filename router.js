d3df029a32920c19ebc427bb62cdb8c05f88e36c
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-01 09:36:12 +0000

    middleware added and router created

---
+++ router.js
@@ -0,0 +1,10 @@
+const express = require("express"); 
+
+const projectModel = require("./data/helpers/projectModel.js");
+
+// import middleware below 
+
+
+const router = express.Router(); 
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
 const server = express();
+const router = require("./router.js");
 
-server.use(express.json());
-server.use(cors());
+server.use(logger);
 server.use(helmet());
+server.use(express.json());
+server.use(cors())
+server.use("/api/projects", router)
+
+// middleware
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.path}`)
+    next()
+}
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "Server is working" });
